07044a9b0fc688d8c7c9c77bcb24642fa05573db
onyoo <roberto9m9@gmail.com>
2016-03-30 17:27:58 +0200

    top-stories in app.js

--- javascript/app/app.js
+++ javascript/app/app.js
@@ -4,7 +4,7 @@ angular
     $stateProvider
       .state('top', {
         url: '/top',
-        templateUrl: 'views/TopStories.html',
+        templateUrl: 'views/top-stories.html',
         controller: 'TopStoriesController as top',
         resolve: {
           posts: function (PostsService) {
