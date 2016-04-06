64db62bbe838e193cb5a1d091f060fa4912210ac
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-06 11:12:47 -0600

    Added paginate functionality to the TopStoriesController

--- js/app/controllers/TopStoriesController.js
+++ js/app/controllers/TopStoriesController.js
@@ -1,6 +1,28 @@
+var STORIES_PER_PAGE = 30;
+
 function TopStoriesController(stories) {
   ctrl = this;
-  ctrl.stories = stories.data;
+  //ctrl.stories = stories.data;
+
+  ctrl.page = 0;
+  ctrl.totalStories = stories.data.length;
+  ctrl.totalPages = Math.ceil(control.totalStories / STORIES_PER_PAGE);
+
+  ctrl.paginateStories = function() {
+    ctrl.stories = stories.data.slice(ctrl.page * STORIES_PER_PAGE, (ctrl.page + 1) * STORIES_PER_PAGE);
+  };
+
+  ctrl.nextPage = function() {
+    ctrl.page++;
+    ctrl.paginateStories();
+  };
+
+  ctrl.previousPage = function() {
+    ctrl.page--;
+    ctrl.paginateStories();
+  };
+
+  ctrl.paginateStories();
 }
 
 
