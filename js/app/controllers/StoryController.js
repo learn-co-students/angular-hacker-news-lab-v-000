8f6886285629c1585d5c4cf311a4132e54228140
pajamaw <pjpeterww@gmail.com>
2016-04-12 12:31:11 -0400

    top Stories posting to page with links and author names

---
+++ js/app/controllers/StoryController.js
@@ -0,0 +1,28 @@
+function StoryController(StoryService) {
+
+  var ctrl = this;
+
+  ctrl.topStoriesId = [];
+  ctrl.topStories = [];
+
+  StoryService
+    .getTopStoriesId()
+    .then(function (res){
+      ctrl.topStoriesId = res.data;
+      for(var i=0; i < ctrl.topStoriesId.length; i++){
+        StoryService.getTopStories(ctrl.topStoriesId[i]).then(function(res){
+          ctrl.topStories.push(res.data);
+          //debugger;
+        });
+      };
+    });
+
+
+
+}
+
+
+
+angular
+  .module('app')
+  .controller('StoryController', StoryController);
