ac6962340be28725bb510c578f9ca4d08e0309a6
pajamaw <pjpeterww@gmail.com>
2016-04-12 14:02:59 -0400

    top stories show using state and uirouter

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,15 @@
 angular 
-  .module('app', []);
+  .module('app', ['ngSanitize', 'ui.router'])
+  .config(function ($stateProvider){
+    $stateProvider
+      .state('topstory', {
+        url: '/topstory',        
+        templateUrl: 'views/topstory.html',
+        controller: 'StoryController as top',
+        resolve: {
+          story: function (StoryService, $stateParams) {
+            StoryService.getTopStories($stateParams.id);
+          }
+        }
+      })
+  });
--- js/app/controllers/StoryController.js
+++ js/app/controllers/StoryController.js
@@ -9,10 +9,11 @@ function StoryController(StoryService) {
     .getTopStoriesId()
     .then(function (res){
       ctrl.topStoriesId = res.data;
+
       for(var i=0; i < ctrl.topStoriesId.length; i++){
+
         StoryService.getTopStories(ctrl.topStoriesId[i]).then(function(res){
           ctrl.topStories.push(res.data);
-          //debugger;
         });
       };
     });
