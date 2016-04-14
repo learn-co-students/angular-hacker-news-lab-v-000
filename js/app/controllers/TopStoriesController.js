78a58b059e14cac9123847799cbbfc2ccd621b70
dannyd4315 <dannyd4315@gmail.com>
2016-04-14 13:18:55 +1000

    Done.

--- js/app/controllers/TopStoriesController.js
+++ js/app/controllers/TopStoriesController.js
@@ -4,6 +4,25 @@ function TopStoriesController (posts) {
 
   this.posts = posts.data;
 
+  this.currentPage = 0
+
+  this.nextPage = function(){
+    ctrl.currentPage ++;
+    ctrl.page();
+  };
+
+  this.previousPage = function(){
+    ctrl.currentPage --;
+    ctrl.page();
+  };
+
+  this.page = function(){
+    var pageCount = ctrl.currentPage * 30;
+    return this.posts.slice(pageCount, pageCount + 30)
+  };
+
+  this.page();
+
 }
 
 angular
