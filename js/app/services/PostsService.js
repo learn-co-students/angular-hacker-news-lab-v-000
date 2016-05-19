33b10d8c221caeeeb9addab6ab05a98a867f916c
MitulMistry <MitulMistryDev@gmail.com>
2016-05-19 11:58:56 -0500

    Item and PostsService working

--- js/app/services/PostsService.js
+++ js/app/services/PostsService.js
@@ -1,13 +1,11 @@
 var API_URL = 'https://hacker-news.firebaseio.com/v0';
 
 function PostsService($http) {
-  this.getTopStories = function () {
-    //grab all the top stories from the URL
+  this.getTopStories = function () { //grab all the top stories from the URL
     return $http.get(API_URL + '/topstories.json');
   };
 
-  this.getPost = function() {
-    //grab information about a single post by it's ID. This should hit https://hacker-news.firebaseio.com/v0/item/{id}.json
+  this.getPost = function(id) { //grab information about a single post by it's ID. This should hit https://hacker-news.firebaseio.com/v0/item/{id}.json
     return $http.get(API_URL + '/item/' + id + '.json');
   }
 }
