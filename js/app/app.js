1ad48c17681fea5c7422167f1473ee584da0fd6a
Brian Holland <beehollander@gmail.com>
2016-05-24 16:12:46 -0500

    BOTH 'specs' passing

--- js/app/app.js
+++ js/app/app.js
@@ -6,9 +6,21 @@ angular
           url: '/top',
           templateUrl: '/views/posts.html',
           controller: 'PostController',
+          controllerAs: 'ctrl',
           resolve:{
             top_stories: function($http){
               return $http.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+            }
+          }
+        })
+        .state('post', {
+          url: '/post/:id',
+          templateUrl: '/views/article.html',
+          controller: 'ArticleController',
+          controllerAs: 'ctrl',
+          resolve:{
+            article: function($http){
+              return $http.get('https://hacker-news.firebaseio.com/v0/item/11761065.json');
             }
           }
         });
--- js/app/controllers/PostController.js
+++ js/app/controllers/PostController.js
@@ -1,6 +1,8 @@
 function PostController(top_stories){
   this.top_stories = top_stories.data;
-  this.fairview = 45;
+  this.start = 0;
+
+  this.top_stories_slice = this.top_stories.slice(this.start, this.start + 30);
 }
 
 angular
