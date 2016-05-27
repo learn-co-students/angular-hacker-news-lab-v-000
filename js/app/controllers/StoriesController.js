4810d411640f5f15c0e6261e095f2f6b03e015c5
Alvinluipo <lualvin1234@gmail.com>
2016-05-27 10:51:15 -0400

    Done.

---
+++ js/app/controllers/StoriesController.js
@@ -0,0 +1,31 @@
+function StoriesController(stories){
+  var ctrl = this;
+
+  var postsPerPage = 30;
+  ctrl.start = 0;
+  ctrl.end = postsPerPage;
+  ctrl.last = stories.data.length;
+
+  top.slicePosts = function(){
+    ctrl.stories = stories.data.slice(ctrl.start, ctrl.end);
+  }
+
+  top.prevPage = function(){
+    ctrl.start -= postsPerPage;
+    ctrl.end -= postsPerPage;
+    ctrl.slicePosts();
+  }
+
+  top.nextPage = function(){
+    ctrl.start += postsPerPage;
+    ctr.end += postsPerPage;
+    ctrl.slicePosts();
+  }
+
+  top.slicePosts();
+
+};
+
+angular
+  .module('app')
+  .controller("StoriesController", StoriesController)
