455cfce16292cc061a59e2e75dca17a8ef0d0412
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-05-31 17:44:21 -0500

    Pagination

--- js/app/controllers/TopStoriesController.js
+++ js/app/controllers/TopStoriesController.js
@@ -1,9 +1,26 @@
 function TopStoriesController(posts) {
+  var POSTS_PER_PAGE = 30;
+
   this.posts = posts.data
-  // console.log(posts);
-  
   this.page = 0;
   this.postSize = posts.data.length;
+  this.totalPages = Math.ceil(this.postSize / POSTS_PER_PAGE);
+
+  this.pagination = function() {
+    this.posts = posts.data.slice(this.page * POSTS_PER_PAGE, (this.page + 1) * POSTS_PER_PAGE);
+  };
+
+  this.nextPage = function() {
+    this.page ++;
+    this.pagination();
+  };
+
+  this.prevPage = function() {
+    this.page --;
+    this.pagination();
+  };
+
+  this.pagination();
 
 }
 
