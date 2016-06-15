a872dc3d145641bf48748b2935903d2ee6c59d93
pacosta613 <pedro.acosta613@gmail.com>
2016-06-15 15:54:57 -0400

    Added service

---
+++ js/app/services/TopStoryService.js
@@ -0,0 +1,15 @@
+function TopStoryService($http){
+  var url = 'https://hacker-news.firebaseio.com/v0/';
+
+  this.getTopStories = function () {
+    return $http.get(url + 'topstories.json');
+  };
+
+  this.getPost = function (id){
+    return $http.get(url + 'item/' + id + '.json');
+  };
+}
+
+angular
+  .module('app')
+  .service('TopStoryService', TopStoryService);
