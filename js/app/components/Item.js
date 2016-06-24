885019ff9c14c8fdc98bb3aa0d14894076b10afb
mhnguyen289 <mhnguyen289@gmail.com>
2016-06-24 14:03:53 -0700

    Done.

---
+++ js/app/components/Item.js
@@ -0,0 +1,20 @@
+angular
+  .module('app')
+  .component('item', Item);
+
+var Item = {
+  templateUrl: 'views/item.html',
+	bindings: {
+		id: '='
+	},
+	controller: function(PostsService) {
+		var ctrl = this;
+
+		PostsService
+			.getPost(this.id)
+			.then(function (res) {
+				ctrl.data = res.data;
+			})
+	},
+	controllerAs: 'item'
+};
---
+++ js/app/controllers/TopStoriesController.js
@@ -0,0 +1,35 @@
+
+
+function TopStoriesController.js(posts){
+	var ctrl = this;
+	ctrl.page = 0
+
+	ctrl.page= 1
+	ctrl.maxPage = Math.ceil(ctrl.totalPosts / 30);
+
+	ctrl.paginatePosts = function () {
+		ctrl.posts = posts.data.slice(ctrl.page * 30, (ctrl.page + 1) * 30);
+	}
+
+	ctrl.nextPage = function() {
+		if (ctrl.page < ctrl.maxPage) {
+			console.log("nextPage called.")
+			ctrl.page++;
+			ctrl.getStoryIds();
+		}
+	}
+
+	ctrl.prevPage = function() {
+		if (ctrl.page > 1) {
+			console.log("nextPage called.")
+			ctrl.page--;
+			ctrl.getStoryIds();
+		}
+	}
+	ctrl.paginatePosts();
+
+}
+
+angular
+	.module('app'
+		.controller('TopController', Topcontroller);
