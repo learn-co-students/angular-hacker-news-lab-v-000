a291b87600b913486408e2617c6f9598e3a3b4d3
davdkm <davd.km@gmail.com>
2016-07-18 12:40:00 -0700

    added item component

--- js/app/components/Item.js
+++ js/app/components/Item.js
@@ -1,5 +1,18 @@
 var Item = {
+  restrict: 'E',
+  templateUrl: 'views/item.html',
+  bindings: {
+    id: '='
+  },
+  controllerAs: 'item',
+  controller: function (PostsService) {
+    var ctrl = this;
 
+    PostsService.getPost(this.id)
+      .then(function (res) {
+        ctrl.data = res.data;
+      });
+  }
 }
 
 angular
