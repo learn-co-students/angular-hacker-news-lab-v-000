9019d451e138aa7340404404b859455e3c04db0b
davdkm <davd.km@gmail.com>
2016-07-18 01:01:37 -0700

    added pagination to top stories controller

--- js/app/controllers/TopStoriesController.js
+++ js/app/controllers/TopStoriesController.js
@@ -1,5 +1,27 @@
 function TopStoriesController(posts) {
-  this.data = posts.data;
+  var ctrl = this;
+
+  ctrl.data = posts.data;
+
+  ctrl.currentPage = 0;
+  ctrl.totalPosts = posts.length;
+  ctrl.totalPages = Math.ceil(ctrl.totalPosts / 30)
+
+  ctrl.paginatePosts = function () {
+    ctrl.posts = posts.data.slice(ctrl.currentPage * 30, (ctrl.currentPage + 1) * 30);
+  }
+
+  ctrl.nextPage = function () {
+    ctrl.currentPage++;
+    ctrl.paginatePosts();
+  }
+
+  ctrl.previousPage = function () {
+    ctrl.currentPage--;
+    ctrl.paginatePosts();
+  }
+
+  ctrl.paginatePosts();
 }
 
 angular
