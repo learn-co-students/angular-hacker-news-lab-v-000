81310cca2670743fbf36657f566f3c07dee39c65
davdkm <davd.km@gmail.com>
2016-07-18 00:18:10 -0700

    posts service gets top stories and single story

--- js/app/services/PostsService.js
+++ js/app/services/PostsService.js
@@ -1,5 +1,13 @@
-function PostsService() {
+var apiLink = 'https://hacker-news.firebaseio.com/v0';
 
+function PostsService($http) {
+  this.getTopStories = function () {
+    return $http.get(apiLink + '/topstories.json');
+  }
+
+  this.getPost = function (id) {
+    return $http.get(apiLink + '/item/' + id + '.json');
+  }
 }
 
 angular
