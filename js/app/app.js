b2bb6c3f5d979dfe6e1d298ece65b0c438fa84bb
thebrianemory <the@brianemory.com>
2016-07-29 11:13:35 -0700

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,9 @@
-angular.module('app', []);
+angular.module('app', ['ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('post', {
+        url: '/',
+        templateUrl: 'views/home.html'
+      });
+      $urlRouterProvider.otherwise('/');
+  })
