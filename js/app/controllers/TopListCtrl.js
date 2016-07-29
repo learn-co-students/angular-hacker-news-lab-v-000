d618f77862c78d8fe1749fee0e65de00e100a622
thebrianemory <the@brianemory.com>
2016-07-28 22:44:10 -0700

    Done.

--- js/app/controllers/TopListCtrl.js
+++ js/app/controllers/TopListCtrl.js
@@ -1,15 +1,27 @@
 function TopListCtrl($scope, $http, GetPostsService) {
   $http.get('https://hacker-news.firebaseio.com/v0/topstories.json').success(function(data) {
     $scope.posts = [];
+    var counter = 1;
     for (var i = 0; i < data.length; i++) {
       this.post = GetPostsService.postInfo(data[i]);
       GetPostsService
         .postInfo(data[i])
         .then(function (res) {
+          res.data.listId = counter;
           $scope.posts.push(res.data);
+          counter += 1;
         });
     }
+
+    // pagination
+    $scope.curPage = 0;
+    $scope.pageSize = 30;
+
+   $scope.numberOfPages = function() {
+     return Math.ceil($scope.posts.length / $scope.pageSize);
+    };
   });
+
 }
 
 angular
