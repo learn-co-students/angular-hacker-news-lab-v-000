5ebca7968b42d70122746b7081a904dd941945c5
McN33ly <rmcneely23@gmail.com>
2016-08-05 18:23:55 -0400

    Done.

---
+++ js/app/controllers/RouteController.js
@@ -0,0 +1,8 @@
+// function RouteController($http) {
+//
+//   $http.$get("https://hacker-news.firebaseio.com/v0/topstories.json")
+// }
+//
+// angular
+//   .module('app')
+//   .controller('RouteController', RouteController)
