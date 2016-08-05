9b1a12d7c8ef82b4bda87342289dcf165757e74c
zachnewburgh <zach.newburgh@gmail.com>
2016-08-05 11:31:12 -0400

    Injected PostsService, called .getPosts, hooked up controller to app

--- js/app/controllers/TopStoriesController.js
+++ js/app/controllers/TopStoriesController.js
@@ -1,3 +1,15 @@
-function TopStoriesController(posts) {
+function TopStoriesController(PostsService) {
+  
   var ctrl = this;
+  var ctrl.posts = '';
+
+  PostsService
+    .getPosts()
+    .then(function(res) {
+      ctrl.posts = res.data;
+    });
 }
+
+angular
+  .module('app')
+  .controller('TopStoriesController', TopStoriesController)
