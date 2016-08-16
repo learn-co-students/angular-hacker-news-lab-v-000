8506209af392369df86695a2be313f7a176f3a0e
zachnewburgh <zach.newburgh@gmail.com>
2016-08-16 13:28:42 -0400

    Added functions to allow pagination, including new ctrl.stories variable

--- js/app/controllers/TopStoriesController.js
+++ js/app/controllers/TopStoriesController.js
@@ -5,18 +5,34 @@ function TopStoriesController(PostsService) {
   ctrl.start = 0;
   ctrl.end = 30;
 
-
-  PostsService
+  returnArticles = function() {
+    PostsService
     .getPosts()
     .then(function(res) {
       ctrl.posts = res.data;
       ctrl.articles = [];
-      ctrl.posts.slice(ctrl.start, ctrl.end).forEach(function(id) {
+      ctrl.posts.forEach(function(id) {
         PostsService.getPost(id).then(function(res) {
           ctrl.articles.push(res.data);
+          ctrl.stories = ctrl.articles.slice(ctrl.start, ctrl.end);
         });
       });
     });
+  }
+
+  returnArticles();
+
+  ctrl.paginateNext = function() {
+    this.start = this.start + 30;
+    this.end = this.end + 30;
+    this.stories = this.articles.slice(this.start, this.end)
+  }
+
+  ctrl.paginatePrevious = function() {
+    this.start = this.start - 30;
+    this.end = this.end - 30;
+    this.stories = this.articles.slice(this.start, this.end)
+  }
 }
 
 angular
