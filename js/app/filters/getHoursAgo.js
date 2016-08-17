6f803d0f8f1cbddd3187b734494993f9b5083724
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-08-17 12:14:32 -0700

    Revert "updates getHoursAgoFilter to display days, months, and years"
    This reverts commit 9736cc598adb4181c1067b39de178da2a62b953f.

--- js/app/filters/getHoursAgo.js
+++ js/app/filters/getHoursAgo.js
@@ -7,31 +7,14 @@ angular
       if (typeof input === 'number') {
         var secondDifference = Date.now()/1000 - input;
         if (secondDifference < 3600) {
-          var minutesAgo = Math.floor(secondDifference/60);
-          return pluralizeTime(minutesAgo, 'minute');
-        } else if (3600 <= secondDifference && secondDifference < 86400) {
-          var hoursAgo = Math.floor(secondDifference/3600);
-          return pluralizeTime(hoursAgo, 'hour'); 
-        } else if (86400 <= secondDifference && secondDifference < 2592000) {
-          var daysAgo = Math.floor(secondDifference/86400);
-          return pluralizeTime(daysAgo, 'day');
-        } else if (2592000 <= secondDifference && secondDifference < 31536000) {
-          var monthsAgo = Math.floor(secondDifference/2592000);
-          return pluralizeTime(monthsAgo, 'month');
+          var minutesAgo = secondDifference/60;
+          return Math.floor(minutesAgo) + ' minutes ago';
         } else {
-          var yearsAgo = Math.floor(secondDifference/31536000);
-          return pluralizeTime(yearsAgo, 'year');
+          var hoursAgo = secondDifference/3600; 
+          return Math.floor(hoursAgo) + ' hours ago'; 
         }
       } else {
         throw new Error('getHoursAgoFilter must be given a number as an input.');
       }
-      
     };
-    function pluralizeTime(number, timeUnit) {
-      if (number === 1) {
-        return number + ' ' + timeUnit + ' ago';
-      } else {
-        return number + ' ' + timeUnit + 's ago';
-      }
-    }
   };
