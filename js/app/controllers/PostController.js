96da3b2b1923c3e41283d1b32fbb57e9e7c92cc5
zachnewburgh <zach.newburgh@gmail.com>
2016-08-18 08:31:46 -0400

    Create comment array by calling .getComment(id) function

--- js/app/controllers/PostController.js
+++ js/app/controllers/PostController.js
@@ -1,5 +1,16 @@
-function PostController(post) {
+function PostController(post, PostsService) {
+  ctrl = this;
+
   this.data = post.data;
+  this.kids = post.data.kids;
+
+  this.comments = [];
+
+  ctrl.kids.forEach(function(id) {
+    PostsService.getComment(id).then(function(res) {
+      ctrl.comments.push(res.data);
+    });
+  });
 }
 
 angular
