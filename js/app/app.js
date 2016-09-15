b7910f40a6314606179ff43ff26d17e8ae97aabe
agdavid <antonio.g.david@gmail.com>
2016-09-15 13:19:22 -0400

    add basic ui-router states 'top' and 'post' to app config

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,15 @@
+function UiRouter($stateProvider) {
+  $stateProvider
+    .state('top', {
+      url: '/top',
+      templateUrl: 'js/app/views/top.html'
+    })
+    .state('post', {
+      url: '/post/:id',
+      templateUrl:'js/app/views/post.html'
+    });
+}
+
 angular
-  .module('app', ['ui.router']);
+  .module('app', ['ui.router'])
+  .config(UiRouter);
