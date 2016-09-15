4a664472f439d451df6e617957ad95b7b1c0980c
agdavid <antonio.g.david@gmail.com>
2016-09-15 16:30:22 -0400

    display individual post objects in Item component

--- js/app/components/Item.js
+++ js/app/components/Item.js
@@ -1,5 +1,19 @@
 var Item = {
+  bindings: {
+    id: '='
+  },
+  templateUrl: 'js/app/views/item.html',
+  controller: function(PostsService) {
+    var ctrl = this;
 
+    PostsService
+      .getPost(ctrl.id)
+      .then(function(response) {
+        ctrl.data = response.data;
+      })
+
+  },
+  controllerAs: 'item'
 }
 
 angular
