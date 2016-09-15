944b0e0f82bfefcb3c382254538f9777b10b7137
agdavid <antonio.g.david@gmail.com>
2016-09-15 15:40:38 -0400

    add pagination to StoriesController

--- js/app/controllers/StoriesController.js
+++ js/app/controllers/StoriesController.js
@@ -2,7 +2,26 @@ function StoriesController(stories) {
 
   var vm = this;
 
-  
+  vm.page = 0;
+  vm.stories_per_page = 30;
+  vm.totalStories = stories.data.length;
+  vm.totalPages = Math.ceil(vm.totalStories/vm.posts_per_page);
+
+  vm.paginateStories = function() {
+    vm.stories = stories.data.slice(vm.page*vm.stories_per_page, (vm.page+1)*vm.stories_per_page);
+  };
+
+  vm.nextPage = function() {
+    vm.page +=1;
+    vm.paginateStories()
+  };
+
+  vm.previousPage = function() {
+    vm.page -=1;
+    vm.paginateStories()
+  };
+
+  vm.paginateStories(); 
 }
 
 angular
