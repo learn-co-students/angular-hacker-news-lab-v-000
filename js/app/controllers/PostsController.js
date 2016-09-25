fc3375722bca51645430d849a8f9faadad1214dc
cassaram09 <cassaram09@gmail.com>
2016-09-24 21:51:00 -0400

    http call to stories in controller working, rendering to page

--- js/app/controllers/PostsController.js
+++ js/app/controllers/PostsController.js
@@ -1,7 +1,22 @@
-function PostsController() {
+function PostsController(HttpService) {
   var vm = this;
 
   vm.name = "hello";
+
+  vm.topStories;
+
+  activate();
+
+  function activate(){
+    getStories();
+  }
+
+  function getStories(){
+    return HttpService.getTopStories().then(function(data){ 
+      debugger
+      return vm.topStories = data.data;
+    })
+  }
 }
 
 angular
---
+++ js/app/services/HttpService.js
@@ -0,0 +1,16 @@
+function HttpService($http) {
+  var service = this;
+
+  service.getTopStories = function(){
+    return $http.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+  }
+
+  service.getStory = function(id){
+    return $http.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json');
+  }
+
+}
+
+angular
+  .module('app')
+  .service('HttpService', HttpService);
