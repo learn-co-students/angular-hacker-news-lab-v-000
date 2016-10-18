af76b41226201f4296fc82a76fa8e09ec53a8676
Nathaniel Miller <dukeoflaser@gmail.com>
2016-10-17 18:21:00 -0700

    Add 'root' state @ '/' and corresponding views/home.html template. Add identification  note to index.html

--- js/app/app.js
+++ js/app/app.js
@@ -1,5 +1,9 @@
 angular
   .module('app', ['ui.router'])
   .config(function($stateProvider){
-
+    $stateProvider
+      .state('root', {
+        url: '/',
+        templateUrl: 'views/home.html'
+      });
   });
