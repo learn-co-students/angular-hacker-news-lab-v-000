42504fc3386d1454154391e8bae06663deba5aaa
evanscloud <dijkstrascloud@gmail.com>
2016-10-18 15:19:35 -0400

    Create service to get top stories

---
+++ js/app/services/PostsService.js
@@ -0,0 +1,9 @@
+function PostsService ($http) {
+  this.getTopStories = function () {
+    return $http.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+  }
+}
+
+angular
+  .module('app')
+  .service('PostsService', PostsService);
