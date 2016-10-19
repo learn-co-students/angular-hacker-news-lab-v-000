820540d9bd329d7802605de367d7b24bfa56ce36
evanscloud <dijkstrascloud@gmail.com>
2016-10-18 22:21:37 -0400

    Add service to get individual post data

--- js/app/services/PostsService.js
+++ js/app/services/PostsService.js
@@ -2,6 +2,9 @@ function PostsService ($http) {
   this.getTopStories = function () {
     return $http.get('https://hacker-news.firebaseio.com/v0/topstories.json');
   }
+  this.getPost = function (id) {
+    return $http.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json');
+  }
 }
 
 angular
