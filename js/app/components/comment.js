7b715811611d1ce15a5de71b58d91d93f88fb2c7
evanscloud <dijkstrascloud@gmail.com>
2016-10-18 22:23:30 -0400

    Create comment component and add script to layout

---
+++ js/app/components/comment.js
@@ -0,0 +1,24 @@
+var Comment = {
+
+  templateUrl: 'views/comment.html',
+  bindings: {
+    id: '='
+  },
+
+  controller: function (PostsService) {
+    var postId = this.id;
+    var comment = this;
+
+    this.post = PostsService.getPost(postId);
+    PostsService
+      .getPost(postId)
+      .then(function (res) {
+        comment.data = res.data;
+      });
+  },
+  controllerAs: 'comment'
+};
+
+angular
+  .module('app')
+  .component('comment', Comment);
