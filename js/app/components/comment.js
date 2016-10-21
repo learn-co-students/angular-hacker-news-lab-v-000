23dc310c13d7cbc50b4964d06eb792b359614255
Nathaniel Miller <dukeoflaser@gmail.com>
2016-10-21 15:11:13 -0700

    Top Level comments are now displayed when visiting and individual story.

--- js/app/components/comment.js
+++ js/app/components/comment.js
@@ -1,3 +1,14 @@
 var Comment = {
+  bindings: { id: '=' },
+  controller: function (TopStories) {
+		var ctrl = this;
 
-}
+		TopStories
+			.getStory(this.id)
+			.then(function (response) {
+				ctrl.data = response.data;
+			})
+	},
+  controllerAs: 'comment',
+	templateUrl: 'views/comment.html'
+};
