6e1f2ef3b7818261121a7db673b045176e18559f
Nathaniel Miller <dukeoflaser@gmail.com>
2016-10-22 18:51:44 -0700

    Clean up HomeController.

--- js/app/controllers/HomeController.js
+++ js/app/controllers/HomeController.js
@@ -5,82 +5,23 @@ function HomeController(stories, $filter){
   ctrl.pageNum = 1;
   ctrl.storyIDs = stories.data;
 
+  ctrl.previous = function(){
+    if(ctrl.pageNum > 1){
+      ctrl.pageNum -= 1;
+    }
+    setPage();
+  }
+
+  ctrl.next = function(){
+    if(ctrl.pageNum < (ctrl.storyIDs.length / storiesPerPage)){
+      ctrl.pageNum += 1;
+    }
+    setPage();
+  }
 
   function setPage(){
     ctrl.pageOfStoryIDs = $filter('paginationFilter')(ctrl.storyIDs, ctrl.pageNum, storiesPerPage);
   }
 
   setPage();
-
-
-
-
-
-
-
-
-  // ctrl.getStory = getStory;
-  // ctrl.filteredList = $filter('paginationFilter')(this.stories, pageNum, 30);
-
-
-  // console.log(getStory);
-
-  // function populateStories(){
-  //   ctrl.pageOfStories = [];
-  //
-  //   ctrl.filteredList.forEach(function(id, index){
-  //     ctrl.getStoryData(id);
-  //   });
-  // }
-
-  // function getStoryData(id){
-  //   getStory(id)
-  //     .then(function(response){
-  //       ctrl.pageOfStories.push(response.data);
-  //     });
-  // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // var pageNum = 4;
-
-
-
-  // function paginateStories(pageNum){
-  //   ctrl.filteredList = $filter('paginationFilter')(this.stories, pageNum, 30);
-  // }
-
-
-
-
-
-  // populateStories();
-
-  this.previous = function(){
-    if(ctrl.pageNum > 1){
-      ctrl.pageNum -= 1;
-    }
-    setPage();
-    console.log('Hey');
-  }
-
-  this.next = function(){
-    if(ctrl.pageNum < (ctrl.storyIDs.length / storiesPerPage)){
-      ctrl.pageNum += 1;
-    }
-    setPage();
-    console.log('Hey!');
-  }
-
 }
