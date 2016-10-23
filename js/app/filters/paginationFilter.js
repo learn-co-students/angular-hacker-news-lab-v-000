ad012d62a41e4fe5a0794b086c0a686297915026
Nathaniel Miller <dukeoflaser@gmail.com>
2016-10-22 19:16:11 -0700

    Clean up pagination filter.

--- js/app/filters/paginationFilter.js
+++ js/app/filters/paginationFilter.js
@@ -1,7 +1,6 @@
 function PaginationFilter(){
   return function(collection, pageNum, pageSize){
     var index = (pageNum * pageSize) - pageSize;
-    console.log(index);
     return collection.slice(index, index + pageSize);
   }
 }
