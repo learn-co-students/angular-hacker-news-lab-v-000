02c35964725cfb50ec6f7cf20a5a0784ac1edb69
mcasciato <michael.casciato@icloud.com>
2016-10-28 11:37:37 -0400

    refactored PostsService

--- js/app/services/PostsService.js
+++ js/app/services/PostsService.js
@@ -1,16 +1,14 @@
-var api_url = 'https://hacker-news.firebaseio.com/v0';
-
 function PostsService($http) {
-  this.getTopStories = function () {
-    return $http.get(api_url + '/topstories.json');
-  };
+    this.getTopStories = function() {
+        return $http.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+    };
 
-  this.getPost = function (id) {
-    return $http.get(api_url + '/item/' + id + '.json');
-  };
+    this.getPost = function(id) {
+        return $http.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json');
+    };
 }
 
 
 angular
-  .module('app')
-  .service('PostsService', PostsService);
+    .module('app')
+    .service('PostsService', PostsService);
