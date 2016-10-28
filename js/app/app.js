fb7101381f7e51b6e954b8239b6f5819729076de
Andreas Papacostas <github email address>
2016-10-29 02:05:42 +0300

    Done.

---
+++ js/app/app.js
@@ -0,0 +1,30 @@
+angular
+  .module('app', ['ui.router', 'ngSanitize'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('post',{
+        url: '/post/:id',
+        templateUrl: 'views/post.html',
+        controller: 'PostsController as post',
+        resolve: {
+          post: function ($stateParams, PostsService){
+            return PostsService.getPost($stateParams.id);
+          }
+        }
+      })
+
+      .state('top', {
+        url: '/top',
+        templateUrl: 'views/index.html',
+        controller: 'TopStoriesController as top',
+        resolve: {
+          posts: function (PostsService){
+            PostsService.getTopStories(); 
+          }
+        }
+      });
+
+    $urlRouterProvider.otherwise('/top');
+    
+  });
---
+++ js/app/controllers/TopStoriesController.js
@@ -0,0 +1,30 @@
+
+var posts_limit = 30
+
+function TopStoriesController(posts){
+  var ctrl = this;
+
+  ctrl.page = 0;
+  ctrl.totalPosts = posts.data.length;
+  ctrl.totalPages = Math.ceil(ctrl.totalPosts / posts_limit);
+
+  ctrl.paginatePosts = function () {
+    ctrl.posts = posts.data.slice(ctrl.page * posts_limit, (ctrl.page + 1) * posts_limit);
+  };
+
+  ctrl.nextPage = function (){
+    ctrl.page ++;
+    ctrl.paginatePosts()
+  }
+
+  ctrl.previousPage = function (){
+    ctrl.page --;
+    ctrl.paginatePosts
+  };
+
+  ctrl.paginatePosts();
+}
+
+angular
+  .module('app')
+  .controller('TopStoriesController', TopStoriesController);
