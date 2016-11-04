49df579005b98b1451a8dc82be9307eb92910b86
cmiller36 <m.calliemiller@gmail.com>
2016-11-03 20:50:33 -0400

    update TopStoriesControllers

--- js/app/controllers/TopStoriesController.js
+++ js/app/controllers/TopStoriesController.js
@@ -1,1 +1,29 @@
-TopStoriesController.js
+var post_limit = 30;
+
+function TopStoriesController(posts) {
+  var ctrl = this;
+
+  ctrl.page = 0;
+  ctrl.totalPosts = posts.data.length;
+  ctrl.totalPages = Math.ceil(ctrl.totalPosts / post_limit);
+
+  ctrl.paginatePosts = function () {
+    ctrl.posts = posts.data.slice(ctrl.page * post_limit, (ctrl.page + 1) * post_limit);
+  };
+
+  ctrl.nextPage = function () {
+
+  };
+
+  ctrl.previousPage = function () {
+
+  };
+
+  ctrl.paginatePosts();
+
+};
+
+
+angular
+  .module('app')
+  .controller('TopStoriesController', TopStoriesController);
