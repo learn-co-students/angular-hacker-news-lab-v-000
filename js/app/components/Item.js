0fedb263735915c024f81d3b191c91120b2243d1
cmiller36 <m.calliemiller@gmail.com>
2016-11-03 21:51:57 -0400

    update item component

--- js/app/components/Item.js
+++ js/app/components/Item.js
@@ -1,14 +1,19 @@
 var Item = {
   templateUrl: 'views/item.html',
   bindings: {
-
+        id: '='
   },
-  controller: {
+  controller: function(PostsService) {
+      var ctrl = this;
 
+      PostsService
+          .getPost(this.id)
+          .then(function (res) {
+              ctrl.data = res.data;
+          });
   },
   controllerAs: 'item'
-
-}
+};
 
 angular
     .module('app')
