d59e51fc1777dea4d7e6e67df293b899a743aa86
cmiller36 <m.calliemiller@gmail.com>
2016-11-03 20:57:33 -0400

    add view/item.html and item component

--- js/app/components/Item.js
+++ js/app/components/Item.js
@@ -1,1 +1,15 @@
-Item.js
+var Item = {
+  templateUrl: 'views/item.html',
+  bindings: {
+
+  },
+  controller: {
+
+  },
+  controllerAs: 'item'
+
+}
+
+angular
+    .module('app')
+    .component('item', Item)
