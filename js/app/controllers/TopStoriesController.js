7425b9c463fb9bac7b0bd33cb11f03af8867cd61
jd2rogers2 <jd2rogers2@gmail.com>
2016-11-14 14:09:54 -0500

    donzo

--- js/app/controllers/TopStoriesController.js
+++ js/app/controllers/TopStoriesController.js
@@ -1,18 +1,22 @@
 function TopStoriesController(stories, PostService){
   var ctrl = this;
   ctrl.currentPage = 1;
-  ctrl.stories_id_array = stories.data.splice((ctrl.currentPage * 30 - 30), 30);
+  ctrl.stories_id_array = stories.data.slice(ctrl.currentPage * 30 - 30, ctrl.currentPage * 30);
   ctrl.stories = stories;
+  ctrl.totalStoriesCount = ctrl.stories.data.length;
 
   ctrl.nextPage = function(){
-    ctrl.currentPage++;
-    ctrl.stories_id_array = stories.data.splice((ctrl.currentPage * 30 - 30), 30);
-    debugger;
+    if ((ctrl.currentPage * 30 - 30) < ctrl.totalStoriesCount){
+      ctrl.currentPage++;
+      ctrl.stories_id_array = stories.data.slice(ctrl.currentPage * 30 - 30, ctrl.currentPage * 30);
+    }
   }
 
   ctrl.prevPage = function(){
-    ctrl.currentPage--;
-    ctrl.stories_id_array = stories.data.splice((ctrl.currentPage * 30 - 30), 30);
+    if (ctrl.currentPage > 1) {
+      ctrl.currentPage--;
+      ctrl.stories_id_array = stories.data.slice(ctrl.currentPage * 30 - 30, ctrl.currentPage * 30);
+    }
   }
 }
 
