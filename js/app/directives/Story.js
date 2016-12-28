1664feeb3718615163a30206a370e43606a9c47a
jbok4 <jbok4@aol.com>
2016-12-28 11:39:57 -0500

    author, time added to view

--- js/app/directives/Story.js
+++ js/app/directives/Story.js
@@ -28,10 +28,36 @@ angular
   function StoryDirectiveController(TopStoriesService) {
     var vm = this;
 
+    function getDomainUrl(url) {
+      var a = document.createElement('a');
+      a.setAttribute('href', url);
+      if (a.hostname === 'news.ycombinator.com') {
+        return '';
+        } else {
+          return '(' + a.hostname + ')';
+        }
+    }
+
+    function getTime(seconds) {
+    var secondDifference = (Date.now()/1000) - seconds;
+    var hoursAgo = secondDifference/3600;
+    return Math.floor(hoursAgo);
+  }
+
     TopStoriesService
-    .getStory(vm.id)
-    .then(function(res) {
-      vm.story = res.data;
-    })
+      .getStory(vm.id)
+      .then(function(res) {
+        vm.story = res.data;
+        vm.title = vm.story.title;
+        vm.url = vm.story.url;
+        if(!vm.url) {
+          vm.url = 'https://news.ycombinator.com/item?id=' + vm.story.id;
+        }
+        vm.domain = getDomainUrl(vm.url);
+        vm.score = vm.story.score;
+        vm.author = vm.story.by;
+        vm.time = getTime(vm.story.time);
+
+      })
   }
 })(); 
