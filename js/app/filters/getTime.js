c746928783c64c7a31eb5b70a0250fcf297173d0
jbok4 <jbok4@aol.com>
2016-12-29 14:01:03 -0500

    getTimeFilter updates to add week, month, year

--- js/app/filters/getTime.js
+++ js/app/filters/getTime.js
@@ -9,13 +9,33 @@ function getTime() {
 
         if (secondDifference < 3600) {
           var minutesAgo = secondDifference/60;
-          return Math.floor(minutesAgo) + ' minutes ago';
-        } else {
+          return pluralizeTime(Math.floor(minutesAgo), 'minute');
+        } else if (3600 <= secondDifference && secondDifference < 86400) {
           var hoursAgo = secondDifference/3600;
-          return Math.floor(hoursAgo) + ' hours ago';
+          return pluralizeTime(Math.floor(hoursAgo), 'hour');
+        } else if (86400 <= secondDifference && secondDifference < 604800) {
+          var daysAgo = secondDifference/86400;
+          return pluralizeTime(Math.floor(daysAgo), 'day');
+        } else if (604800 <= secondDifference && secondDifference < 2678400) {
+          var weeksAgo = secondDifference/604800;
+          return pluralizeTime(Math.floor(weeksAgo), 'week');
+        } else if (2678400 <= secondDifference && secondDifference < 31536000) {
+          var monthsAgo = secondDifference/2678400;
+          return pluralizeTime(Math.floor(monthsAgo), 'month');
+        } else {
+          var yearsAgo = secondDifference/31536000;
+          return pluralizeTime(Math.floor(yearsAgo), 'year');
         }
       } else {
-        throw new Error('getTimeFilter must be given a number as an input.');
+        throw new Error('getTimeFilter must be given a number as an input');
       }
     };
+
+    function pluralizeTime(time, timeUnit) {
+      if(time === 1) {
+        return time + ' ' + timeUnit + ' ago';
+      } else {
+        return time + ' ' + timeUnit + 's ago';
+      }
+    }
   };
