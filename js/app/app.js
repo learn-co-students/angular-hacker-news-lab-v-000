5f91c01d3c0adc14d0bb77c834378e71cb0a4b8e
karuna24s <karuna24s@gmail.com>
2016-12-31 09:20:12 -0500

    created hacker news clone

---
+++ js/app/app.js
@@ -0,0 +1,29 @@
+angular
+  .module('app', ['ui.router', 'ngSanitize'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('top', {
+        url: '/top',
+        templateUrl: 'views/top-stories.html',
+        controller: 'TopStoriesController as topStories',
+        resolve: {
+          topStories: function(TopStoriesService){
+            return TopStoriesService.getTopStories();
+          }
+        }
+      })
+
+      .state('post', {
+        url: '/post/:id',
+        templateUrl: 'views/post.html',
+        controller: 'PostController as post',
+        resolve: {
+          post: function($stateParams, TopStoriesService){
+            return TopStoriesService.getPost($stateParams.id);
+          }
+        }
+      });
+
+      $urlRouterProvider.otherwise('/top');
+
+  })
---
+++ js/app/controllers/TopStoriesController.js
@@ -0,0 +1,29 @@
+function TopStoriesController(topStories) {
+  var STORIES_PER_PAGE = 30;
+  var ctrl = this;
+
+  ctrl.page = 0;
+  ctrl.totalPosts = topStories.data.length;
+  ctrl.totalPages = Math.ceil(ctrl.totalPosts / STORIES_PER_PAGE);
+
+  ctrl.paginatePosts = function(){
+    ctrl.topStories = topStories.data.slice(ctrl.page * STORIES_PER_PAGE, (ctrl.page + 1) * STORIES_PER_PAGE );
+  }
+
+  ctrl.previousPage = function(){
+    ctrl.page--;
+    ctrl.paginatePosts();
+  }
+  ctrl.nextPage = function(){
+    ctrl.page++;
+    ctrl.paginatePosts();
+  }
+
+  ctrl.paginatePosts();
+
+}
+
+
+angular
+  .module('app')
+  .controller('TopStoriesController', TopStoriesController);
