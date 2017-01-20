bc9ac35a7135d9bad6adbc14d4201fb8b2326bac
Han Lee <ralphgoodwin@Ralphs-MacBook-Air.local>
2017-01-20 15:02:50 -0500

    stuck at creating resolve

--- js/app/app.js
+++ js/app/app.js
@@ -1,18 +1,23 @@
 angular
     .module('app', ['ngSanitize', 'ui.router'])
     .config(function ($stateProvider) {
-
       $stateProvider
         .state('top', {
           url: '/top',
-          templateUrl: 'views/top.html',
-          controller: 'NewsCtrl as news',
-          resolve: {
-            //   userData: function ($stateParams, UserService) {
-            //       console.log($stateParams.id);
-            //       return UserService.getUser($stateParams.id);
-            //   }
-          }
+          templateUrl: 'views/top.html'
+          // controller: 'NewsCtrl'// as news' ,
+          // resolve: {
+              //
+              // newsList: function (NewsService) {
+              //     return NewsService.getNewsId();
+              // }
+            //   NewsService
+            //     .getNewsId()
+            //     .then(function (res) {
+            //       ctrl.stories = res.data;
+            //       console.log(ctrl.stories);
+            //     });
+          // }
         });
 
   });
--- js/app/controllers/NewsCtrl.js
+++ js/app/controllers/NewsCtrl.js
@@ -1,19 +1,40 @@
-function NewsCtrl($scope, NewsService) {
+function NewsCtrl($scope) {//newsList) {
   $scope.text = 'Hello, testing';
+  // console.log(this);
+  // this.stories = newsList.data;
 
-  NewsService
-    .getNewsId()
-    .then(function (res) {
-      // console.log(res.data)
-    });
 
-  NewsService
-    .getNews(13439828)
-    .then( function (news) {
-      console.log(news.data);
-    });
+
+  // NewsService
+  //   .getNews(13439828)
+  //   .then( function (news) {
+  //     console.log(news.data);
+  //   });
 }
 
 angular
 	.module('app')
 	.controller('NewsCtrl', NewsCtrl);
+
+
+
+  // function NewsCtrl($scope, NewsService) {
+  //   $scope.text = 'Hello, testing';
+  //
+  //   var ctrl = this;
+  //
+  //   ctrl.stories = [];
+  //
+  //   NewsService
+  //     .getNewsId()
+  //     .then(function (res) {
+  //       ctrl.stories = res.data;
+  //       console.log(ctrl.stories);
+  //     });
+  //
+  //   NewsService
+  //     .getNews(13439828)
+  //     .then( function (news) {
+  //       console.log(news.data);
+  //     });
+  // }
