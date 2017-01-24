a8817fa29c2620e82ab5ce056d915d5714254460
tpstar <hanlee12@gmail.com>
2017-01-24 08:17:42 -0500

    stuck at previous next button show hide

--- js/app/controllers/NewsCtrl.js
+++ js/app/controllers/NewsCtrl.js
@@ -1,26 +1,21 @@
 function NewsCtrl($scope, newsList) {
-  // $scope.text = 'Hello, testing';
-  // this.stories = newsList.data;
+
   var ctrl = this;
   var nbStoryPerPage = 30;
-  var currPage = 1;
+  $scope.currPage = 1;
 
-  // this.startStoryNo = currPage*nbStoryPerPage - nbStoryPerPage + 1;
-  // console.log(this.startStoryNo);
-  // this.stories = newsList.data.slice(this.startStoryNo - 1, this.startStoryNo + nbStoryPerPage - 1);
 
   ctrl.displayPage = function() {
-    ctrl.startStoryNo = currPage*nbStoryPerPage - nbStoryPerPage + 1;
-    console.log(ctrl.startStoryNo);
+    ctrl.startStoryNo = $scope.currPage*nbStoryPerPage - nbStoryPerPage + 1;
     ctrl.stories = newsList.data.slice(ctrl.startStoryNo - 1, ctrl.startStoryNo + nbStoryPerPage - 1);
   };
   $scope.next = function() {
-    currPage++;
-    console.log(currPage);
+    $scope.currPage++;
+    console.log($scope.currPage);
     ctrl.displayPage();
   };
   $scope.previous = function() {
-    currPage--;
+    $scope.currPage--;
     ctrl.displayPage();
   };
 
