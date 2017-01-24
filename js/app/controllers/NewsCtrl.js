0b1af6838cbb40bac83f40454b68b08ea3974597
tpstar <hanlee12@gmail.com>
2017-01-23 22:00:35 -0500

    add working prev next button

--- js/app/controllers/NewsCtrl.js
+++ js/app/controllers/NewsCtrl.js
@@ -1,16 +1,30 @@
-function NewsCtrl(newsList) {
+function NewsCtrl($scope, newsList) {
   // $scope.text = 'Hello, testing';
   // this.stories = newsList.data;
+  var ctrl = this;
+  var nbStoryPerPage = 30;
+  var currPage = 1;
 
- var noStoriesPerPage = 30;
- var currPage = 1;
+  // this.startStoryNo = currPage*nbStoryPerPage - nbStoryPerPage + 1;
+  // console.log(this.startStoryNo);
+  // this.stories = newsList.data.slice(this.startStoryNo - 1, this.startStoryNo + nbStoryPerPage - 1);
 
- currPage += 2;
- this.startStoryNo = currPage*noStoriesPerPage - noStoriesPerPage + 1;
- console.log(this.startStoryNo);
- this.stories = newsList.data.slice(this.startStoryNo - 1, this.startStoryNo + noStoriesPerPage - 1);
- // console.log(this.stories);
+  ctrl.displayPage = function() {
+    ctrl.startStoryNo = currPage*nbStoryPerPage - nbStoryPerPage + 1;
+    console.log(ctrl.startStoryNo);
+    ctrl.stories = newsList.data.slice(ctrl.startStoryNo - 1, ctrl.startStoryNo + nbStoryPerPage - 1);
+  };
+  $scope.next = function() {
+    currPage++;
+    console.log(currPage);
+    ctrl.displayPage();
+  };
+  $scope.previous = function() {
+    currPage--;
+    ctrl.displayPage();
+  };
 
+  ctrl.displayPage();
 }
 
 angular
