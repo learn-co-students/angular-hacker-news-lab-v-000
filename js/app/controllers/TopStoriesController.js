92f3a32deaa66ea85496c6a1732292b541a8f488
radditude <clare.e.horton@gmail.com>
2017-02-27 20:57:00 +0000

    API calls functioning

--- js/app/controllers/TopStoriesController.js
+++ js/app/controllers/TopStoriesController.js
@@ -1,14 +1,21 @@
 function TopStoriesController(StoriesService) {
     var vm = this;
-    
-    vm.aName = "N";
+    vm.topStoriesIDs = [];
+    vm.topStoriesInfo = [];
+    vm.aStory;
     
     StoriesService
         .getTopStoriesIDs()
         .then(function(res) {
-            vm.aName = res.data[0]; 
+            vm.topStoriesIDs = res.data;
+            var one = vm.topStoriesIDs[0]
+            vm.topStoriesIDs.forEach(function(id) {
+                StoriesService.getStoryByID(id).then(function(res) {
+                    vm.topStoriesInfo.push(res.data);
+                });
+            });
+            console.log(vm.topStoriesInfo);
         });
-    
 }
 
 angular
--- js/app/services/StoriesService.js
+++ js/app/services/StoriesService.js
@@ -2,6 +2,10 @@ function StoriesService($http) {
     this.getTopStoriesIDs = function() {
         return $http.get('https://hacker-news.firebaseio.com/v0/topstories.json');
     }
+    
+    this.getStoryByID = function(id) {
+        return $http.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json');
+    }
 }
 
 angular
