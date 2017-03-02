e81467bd044df49eae173f0c45e7b60bb3035471
Clare <clare.e.horton@gmail.com>
2017-03-02 01:59:24 -0600

    refactoring TopStoriesController

--- js/app/controllers/TopStoriesController.js
+++ js/app/controllers/TopStoriesController.js
@@ -9,33 +9,23 @@ function TopStoriesController(StoriesService) {
         .getTopStoriesIDs()
         .then(function(res) {
             vm.topStoriesIDs = res.data;
-            vm.thisPageIDs = vm.topStoriesIDs.slice(0,30);
-            vm.thisPageIDs.forEach(function(id, index) {
-              StoriesService.getStoryByID(id).then(function(res) {
-                var data = res.data;
-                data.rank = index;
-                vm.thisPage.push(data);
-              });
-            });
+            loadPage(0, 30);
           });
 
     vm.getNextPage = function() {
       vm.counter += 30;
       vm.thisPage = [];
-      vm.thisPageIDs = vm.topStoriesIDs.slice(vm.counter, vm.counter + 29);
-      vm.thisPageIDs.forEach(function(id, index) {
-        StoriesService.getStoryByID(id).then(function(res) {
-          var data = res.data;
-          data.rank = index;
-          vm.thisPage.push(data);
-        });
-      });
+      loadPage(vm.counter, vm.counter + 29);
     };
 
     vm.getPreviousPage = function() {
       vm.counter -= 30;
       vm.thisPage = [];
-      vm.thisPageIDs = vm.topStoriesIDs.slice(vm.counter, vm.counter + 29);
+      loadPage(vm.counter, vm.counter + 29);
+    };
+
+    function loadPage(start, end) {
+      vm.thisPageIDs = vm.topStoriesIDs.slice(start,end);
       vm.thisPageIDs.forEach(function(id, index) {
         StoriesService.getStoryByID(id).then(function(res) {
           var data = res.data;
